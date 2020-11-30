77b1b9fe8d26924adf7e649bd7bd2c0fc14825be
MaOndrova <68665487+MaOndrova@users.noreply.github.com>
2020-11-30 20:23:01 +0100

    .

--- Todolist/script.js
+++ Todolist/script.js
@@ -1,17 +1,41 @@
+document.getElementById('datum').valueAsDate = new Date();
+
+
+
 
 let count = 1;
 $("#tlacitko").on("click", () => {
     count++;
     let komentar = $("#ukol").val();
-    $("ol").append("<li class='task' id=todo"+count+">"+komentar+"</li>");
-    // $("#todo"+count).append("<img src='bin.png' width=30>");
-    // $("#todo"+count).append("<img src='select.png' width=30 class='button-erase' id='button"+count+"'>");
-    $("#todo"+count).append("<button class='button-erase' id='button"+count+"'>Done</button>");
+    let den = $("#datum").val()
+
+    var obj = {
+      text: komentar,
+        datum: den,
+    }    
+    localStorage.setItem("itemname"+count, JSON.stringify(obj)); // Save the obj as string
+    var item = JSON.parse(localStorage.getItem("itemname"+count));
+    $("ol").append("<li class='task' id=todo"+count+">"+item.datum+" " +item.text+"</li>");
+    $("#todo"+count).append("<button class='button-erase' id="+count+">Done</button>");
+    console.log(item.datum); 
   });
+
+ 
+// function createMenuItem(name) {
+//   let li = document.createElement('li');
+//   li.textContent = name;
+//   return li;
+// }
+
+// document.getElementById('tlacitko').addEventListener('click', function(){
+//   let komentar = document.getElementById("ukol").value;
+//   let den = document.getElementById('datum').value;
+//   const menu = document.querySelector('#list');
+//   menu.appendChild(createMenuItem(den + ' '+ komentar));
+// });
 
   $('.container-list').on('click', '.button-erase', (event) => {
   var radek = event.target.id;
-  console.log(radek);
   $("#"+radek).closest('.task').remove();
   $("#"+radek).remove();
 
