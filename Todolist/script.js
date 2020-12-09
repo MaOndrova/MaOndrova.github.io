030296d1e860ceddac972405559ef6ae73f732ff
MaOndrova <68665487+MaOndrova@users.noreply.github.com>
2020-12-09 18:43:09 +0100

    adding pallet icon, correcting functions

--- Todolist/script.js
+++ Todolist/script.js
@@ -2,72 +2,81 @@ let number = 0;
 
 $(document).ready(function(){
  //localStorage.removeItem("pole");
-  document.getElementById('datum').valueAsDate = new Date();
+  document.getElementById('date').valueAsDate = new Date();
   reload();
+  reloadColor();
 });
 
 function reload(){
+  number = 0;
   pole = JSON.parse(localStorage.getItem('pole')) || [];
-  pole.forEach(myFunction);}
+  pole.forEach(addinglist);
+}
 
 
-function myFunction(item, index) {
-  $(".list").append("<li class='task' id=todo"+index+">"+item+" "+"</li>");
-  $("#todo"+index).append("<button class='button-erase' id="+index+"><img src='right-tick.svg' alt='Done' width='17px'></button>");
-  number = index;
+function addinglist(item) {
+  $(".list").append("<li class='task' id=todo"+number+">"+item+" "+"</li>");
+  $("#todo"+number).append("<button class='button-erase' id="+number+"></button>");
+  number++;
 } 
 
-function clear(){
-    $('.list li').each(function() {
-    $(this).remove();
-  }) 
-}
-
-function SaveDataToLocalStorage(data)
-{
-    pole = JSON.parse(localStorage.getItem('pole')) || [];
-    pole.push(data);
-    localStorage.setItem('pole', JSON.stringify(pole));
-}
-
-
-$("#tlacitko").on("click", () => {
-    number++;
-    let komentar = $("#ukol").val();
-    let den = $("#datum").val()   
-    SaveDataToLocalStorage(den + " " + komentar);
-    $(".list").append("<li class='task' id=todo"+number+">"+den+" " +komentar+"</li>");
-    $("#todo"+number).append("<button class='button-erase' id="+number+"><img src='right-tick.svg' alt='Done' width='17px'></button>");
-  });
-
-  function RemoveItem(id){
-    pole = JSON.parse(localStorage.getItem('pole')) || [];
-    deletedItem = pole[id];
-
-    if (id > -1) {
-      pole.splice(id, 1);
-    }
-    localStorage.setItem('pole', JSON.stringify(pole));
-    clear();
-    reload();
-
+function reloadColor(){
+  color =  JSON.parse(localStorage.getItem('color-box'));
+  if (color != ''){
+    document.getElementById('main').style.backgroundColor = color;
+    document.getElementById('todocolor').value = color;
   }
+}
 
-  $('.container-list').on('click', '.button-erase', (event) => {
-  var radek = event.target.id;
-  RemoveItem(radek);
-  $(" li #todo"+radek).remove();  
+function clear(){
+  $('.list li').each(function() {
+  $(this).remove();
+  }); 
+}
+
+function saveDataToLocalStorage(data)
+{
+  pole = JSON.parse(localStorage.getItem('pole')) || [];
+  pole.push(data);
+  localStorage.setItem('pole', JSON.stringify(pole));
+}
+
+
+$("#btn").on("click", () => { 
+  let note = $("#inputTask").val();
+  let date = $("#date").val()   
+  saveDataToLocalStorage(date + " " + note);
+  addinglist(date + " " + note);
+});
+
+function removeItem(id){
+  pole = JSON.parse(localStorage.getItem('pole')) || [];
+
+  if (id > -1) {
+    pole.splice(id, 1);
+  }
+  console.table(pole);
+  localStorage.setItem('pole', JSON.stringify(pole));
+  clear();
+  reload();
+}
+
+$('.container-list').on('click', '.button-erase', (event) => {
+  var row = event.target.id;
+  removeItem(row);
+  $(" li #todo"+row).remove();  
 });
 
 // Changing background color
 document.getElementById('todocolor').addEventListener('change', function(){
   let color = this.value;
+  localStorage.setItem('color-box', JSON.stringify(color));
   document.getElementById('main').style.backgroundColor = color;
 });
 
 //Shutdown bubling
 $(".btn_buble").on('click', function(){
-  $('.buble3, .buble4').toggleClass(' bubbling34');
   $('.buble1, .buble2').toggleClass(' bubbling12');
+  $('.buble3, .buble4').toggleClass(' bubbling34');
   $('.buble5, .buble6').toggleClass(' bubbling56');
 });
